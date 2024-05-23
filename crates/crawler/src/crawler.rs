accd4560d95e2034133f0b309e3c3ab9abbb9927
raphael <raphael.pellet@protonmail.com>
2024-05-23 18:07:28 +0200

    :zap: add debug, comment and change scraper

--- crates/crawler/src/crawler.rs
+++ crates/crawler/src/crawler.rs
@@ -1,11 +1,10 @@
+use crate::spiders::Spider;
 use futures::stream::StreamExt;
 use std::sync::atomic::Ordering;
 use std::sync::{atomic::AtomicUsize, Arc};
 use std::{collections::HashSet, time::Duration};
 use tokio::sync::{mpsc, Barrier};
 use tokio::time::sleep;
-
-use crate::spiders::Spider;
 
 pub struct Crawler {
     delay: Duration,
@@ -18,8 +17,8 @@ pub struct Crawler {
 impl Crawler {
     pub fn new(
         delay: Duration,
-        crawling_concurrency: usize,
-        processing_concurrency: usize,
+        crawling_concurrency: usize,   // urls numbers
+        processing_concurrency: usize, // items numbers
     ) -> Self {
         Self {
             delay,
@@ -33,43 +32,46 @@ impl Crawler {
 
 impl Crawler {
     pub async fn run<T: Send + 'static>(&self, spider: Arc<dyn Spider<Item = T>>) {
+        // queue capacity
+        let crawling_queue_capacity = self.crawling_concurrency * 400;
+        let processing_queue_capacity = self.processing_concurrency * 10;
+
+        // create counters
         let mut visited_urls: HashSet<String> = HashSet::new();
         let active_spiders = Arc::new(AtomicUsize::new(0));
 
-        // crawling queue capacity
-        let (urls_to_visit_tx, urls_to_visit_rx) = mpsc::channel(self.crawling_concurrency * 400);
-        // processing queue capacity
-        let (items_tx, items_rx) = mpsc::channel(self.processing_concurrency * 10);
+        // create channels
+        let (urls_to_visit_tx, urls_to_visit_rx) = mpsc::channel(crawling_queue_capacity);
+        let (items_tx, items_rx) = mpsc::channel(processing_queue_capacity);
+        let (new_urls_tx, mut new_urls_rx) = mpsc::channel(crawling_queue_capacity);
 
-        let (new_urls_tx, mut new_urls_rx) = mpsc::channel(self.crawling_concurrency * 400);
+        // set a barrier limit
         let barrier = Arc::new(Barrier::new(3));
 
+        // insert the first urls
         for url in spider.start_urls() {
             visited_urls.insert(url.clone());
             let _ = urls_to_visit_tx.send(url).await;
         }
 
-        self.launch_processors(
-            self.processing_concurrency,
-            spider.clone(),
-            items_rx,
-            barrier.clone(),
-        );
+        // send outputs to processing
+        self.launch_processors(spider.clone(), items_rx, barrier.clone());
 
+        // send urls to visit and queue items & new urls
         self.launch_scrapers(
-            self.crawling_concurrency,
             spider.clone(),
             urls_to_visit_rx,
             new_urls_tx.clone(),
             items_tx,
             active_spiders.clone(),
-            self.delay,
             barrier.clone(),
         );
 
+        // control loop
+        println!(">> start control loop");
         loop {
-            println!(">>loop");
-            if let Some((visited_url, new_urls)) = new_urls_rx.try_recv().ok() {
+            // when receive new urls
+            if let Some((visited_url, mut new_urls)) = new_urls_rx.try_recv().ok() {
                 visited_urls.insert(visited_url);
 
                 println!(">> news url: {:?}", new_urls.len());
@@ -78,15 +80,19 @@ impl Crawler {
                 println!(">> news url_2: {:?}", new_urls[2]);
                 println!(">> news url_1383: {:?}", new_urls[1383]);
                 println!(">> news url_1384: {:?}", new_urls[1384]);
-                break;
+                // break;
+                let mut new_urls: Vec<String> = new_urls[0..5].to_vec();
+                println!(">> news url: {:?}", new_urls.len());
+                let new_urls = vec!["https://www.cvedetails.com/cve/CVE-2024-35996/".to_string()];
                 for url in new_urls {
                     if !visited_urls.contains(&url) {
                         visited_urls.insert(url.clone());
                         println!(">> queuing: {:?}", url);
                         let _ = urls_to_visit_tx.send(url).await;
+                    } else {
+                        println!(">> already visited: {:?}", url);
                     }
                 }
-                println!(">> visited url: {:?}", visited_urls);
             }
 
             if new_urls_tx.capacity() == self.crawling_concurrency * 400
@@ -103,44 +109,55 @@ impl Crawler {
         println!(">> crawler: control loop exited");
         drop(urls_to_visit_tx);
         barrier.wait().await;
+        println!(">> control loop barrier passed");
     }
 
     fn launch_processors<T: Send + 'static>(
         &self,
-        concurrency: usize,
         spider: Arc<dyn Spider<Item = T>>,
-        items: mpsc::Receiver<T>,
+        items_rx: mpsc::Receiver<T>,
         barrier: Arc<Barrier>,
     ) {
+        let concurrency = self.processing_concurrency;
+
+        println!(">> launch processor");
         tokio::spawn(async move {
-            tokio_stream::wrappers::ReceiverStream::new(items)
+            tokio_stream::wrappers::ReceiverStream::new(items_rx)
                 .for_each_concurrent(concurrency, |item| async {
                     let _ = spider.process(item).await;
                 })
                 .await;
 
+            // wait here others barrier before continuing
             barrier.wait().await;
+            println!("launch_processor barrier ended");
         });
     }
 
     fn launch_scrapers<T: Send + 'static>(
         &self,
-        concurrency: usize,
         spider: Arc<dyn Spider<Item = T>>,
         urls_to_visit: mpsc::Receiver<String>,
         new_urls_tx: mpsc::Sender<(String, Vec<String>)>,
         items_tx: mpsc::Sender<T>,
         active_spiders: Arc<AtomicUsize>,
-        delay: Duration,
         barrier: Arc<Barrier>,
     ) {
+        let concurrency = self.crawling_concurrency;
+        let delay = self.delay;
+
+        println!(">> launch scrapers");
         tokio::spawn(async move {
             tokio_stream::wrappers::ReceiverStream::new(urls_to_visit)
                 .for_each_concurrent(concurrency, |queued_url| {
                     let queued_url = queued_url.clone();
                     async {
+                        println!(">> increment active spider");
                         active_spiders.fetch_add(1, Ordering::SeqCst);
+
                         let mut urls = Vec::new();
+
+                        // visit url and get result
                         let res = spider
                             .scrape(queued_url.clone())
                             .await
@@ -150,22 +167,34 @@ impl Crawler {
                             })
                             .ok();
 
+                        // send items if any
                         if let Some((items, new_urls)) = res {
+                            println!(
+                                ">> scraper result: items: {:?}, new urls: {:.}",
+                                items.len(),
+                                new_urls.len()
+                            );
                             for item in items {
                                 let _ = items_tx.send(item).await;
                             }
                             urls = new_urls;
                         }
 
+                        // send new urls if any
                         let _ = new_urls_tx.send((queued_url, urls)).await;
+
+                        // wait and close active spider
                         sleep(delay).await;
+                        println!(">> decrement active spider");
                         active_spiders.fetch_sub(1, Ordering::SeqCst);
                     }
                 })
                 .await;
 
+            // drop items receiver before waiting for other barrier
             drop(items_tx);
             barrier.wait().await;
+            println!(">> launch_scrapper ended");
         });
     }
 }
--- crates/crawler/src/spiders/cve_details.rs
+++ crates/crawler/src/spiders/cve_details.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 use async_trait::async_trait;
 use reqwest::Client;
 use select::document::Document;
-use select::predicate::{Attr, Class, Name, Predicate};
+use select::predicate::{self, Attr, Class, Element, Name, Predicate, Text};
 
 use crate::Result;
 
@@ -74,84 +74,167 @@ impl Spider for CveDetailsSpider {
     }
 
     async fn scrape(&self, url: String) -> Result<(Vec<Self::Item>, Vec<String>)> {
+        // http request
         println!(">> visiting: {:?}", url);
-
         let res = self.http_client.get(url).send().await?.text().await?;
-        let mut items = Vec::new();
-
         let document = Document::from(res.as_str());
 
-        let rows = document.find(Attr("id", "vulnslisttable").descendant(Class("srrowns")));
-        // let rows = document.select(Attr("id", "vulnslisttable").descendant(Class("srrowns")));
-        for row in rows {
-            let mut columns = row.find(Name("td"));
+        // find each CVE link
+        let mut cve_links: Vec<String> = document
+            .find(Attr("id", "searchresults").descendant(Attr("data-tsvfield", "cveinfo")))
+            .filter_map(|n| {
+                n.find(Attr("data-tsvfield", "cveId").descendant(Name("a")))
+                    .next()
+                    .unwrap()
+                    .attr("href")
+            })
+            .map(normalize_url)
+            .collect();
 
-            let _ = columns.next();
-            let cve_link = columns.next().unwrap().find(Name("a")).next().unwrap();
-            let cve_name = cve_link.text().trim().to_string();
-            let cve_url = self.normalize_url(cve_link.attr("href").unwrap());
-
-            let cwe = columns
-                .next()
-                .unwrap()
-                .find(Name("a"))
-                .next()
-                .map(|cvec_link| {
-                    (
-                        cvec_link.text().trim().to_string(),
-                        self.normalize_url(cvec_link.attr("href").unwrap()),
-                    )
-                });
-
-            let _ = columns.next(); // # of exploits column
-
-            let vulnerability_type = columns.next().unwrap().text().trim().to_string();
-            let publish_date = columns.next().unwrap().text().trim().to_string();
-            let update_date = columns.next().unwrap().text().trim().to_string();
-            let score: f32 = columns
-                .next()
-                .unwrap()
-                .text()
-                .trim()
-                .to_string()
-                .parse()
-                .unwrap();
-
-            let _ = columns.next(); // Gained Access Level  column
-
-            let access = columns.next().unwrap().text().trim().to_string();
-            let complexity = columns.next().unwrap().text().trim().to_string();
-            let authentication = columns.next().unwrap().text().trim().to_string();
-            let confidentiality = columns.next().unwrap().text().trim().to_string();
-            let integrity = columns.next().unwrap().text().trim().to_string();
-            let availability = columns.next().unwrap().text().trim().to_string();
-
-            let cve = Cve {
-                name: cve_name,
-                url: cve_url,
-                cwe_id: cwe.as_ref().map(|cwe| cwe.0.clone()),
-                cwe_url: cwe.as_ref().map(|cwe| cwe.1.clone()),
-                vulnerability_type,
-                publish_date,
-                update_date,
-                score,
-                access,
-                complexity,
-                authentication,
-                confidentiality,
-                integrity,
-                availability,
-            };
-            items.push(cve);
-        }
-
+        // find links to others pages
         let next_pages_links: Vec<String> = document
             .find(Attr("id", "pagingb").descendant(Name("a")))
             .filter_map(|n| n.attr("href"))
             .map(normalize_url)
             .collect();
 
-        Ok((items, next_pages_links))
+        cve_links.extend(next_pages_links);
+
+        // Strucure data into items
+        let mut items = Vec::new();
+
+        // extract name
+        let name = if let Some(title) = document
+            .find(Attr("id", "cvedetails-title-div").descendant(Name("a")))
+            .next()
+        {
+            title.text()
+        } else {
+            return Ok((items, cve_links));
+        };
+        println!("Scrap a CVE page");
+        println!("{:?}", name);
+
+        // extract dates
+        let dates: Vec<String> = document
+            .find(Name("main").descendant(Name("span")))
+            .filter_map(|span| span.parent())
+            .filter_map(|div| {
+                if div.text().contains("Published") {
+                    Some(div.text().replace("Published", "").trim().to_string())
+                } else if div.text().contains("Updated") {
+                    Some(div.text().replace("Updated", "").trim().to_string())
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        println!("{:?}", dates);
+
+        // extract details
+        let mut details_node = document
+            .find(Attr("id", "cvss_details_row_1").descendant(Name("div")))
+            .next()
+            .unwrap()
+            .children();
+
+        let attack_vector = details_node.find_map(|div| {
+            if div.text().contains("Attack Vector") {
+                Some(div.text().replace("Attack Vector:", "").trim().to_string())
+            } else {
+                None
+            }
+        });
+
+        println!("{:?}", attack_vector);
+
+        println!("END CVE page");
+
+        // pub struct Cve {
+        //     cwe_id: Option<String>,
+        //     cwe_url: Option<String>,
+        //     vulnerability_type: String,
+        //     score: f32,
+        //     access: String,
+        //     complexity: String,
+        //     authentication: String,
+        //     confidentiality: String,
+        //     integrity: String,
+        //     availability: String,
+        // }
+
+        // let rows = document.find(Attr("id", "cvedetails-title-div").descendant(Class("srrowns")));
+        // for row in rows {
+        //     let mut columns = row.find(Name("td"));
+
+        //     let _ = columns.next();
+        //     let cve_link = columns.next().unwrap().find(Name("a")).next().unwrap();
+        //     let cve_name = cve_link.text().trim().to_string();
+        //     let cve_url = self.normalize_url(cve_link.attr("href").unwrap());
+
+        //     let cwe = columns
+        //         .next()
+        //         .unwrap()
+        //         .find(Name("a"))
+        //         .next()
+        //         .map(|cvec_link| {
+        //             (
+        //                 cvec_link.text().trim().to_string(),
+        //                 self.normalize_url(cvec_link.attr("href").unwrap()),
+        //             )
+        //         });
+
+        //     let _ = columns.next(); // # of exploits column
+
+        //     let vulnerability_type = columns.next().unwrap().text().trim().to_string();
+        //     let publish_date = columns.next().unwrap().text().trim().to_string();
+        //     let update_date = columns.next().unwrap().text().trim().to_string();
+        //     let score: f32 = columns
+        //         .next()
+        //         .unwrap()
+        //         .text()
+        //         .trim()
+        //         .to_string()
+        //         .parse()
+        //         .unwrap();
+
+        //     let _ = columns.next(); // Gained Access Level  column
+
+        //     let access = columns.next().unwrap().text().trim().to_string();
+        //     let complexity = columns.next().unwrap().text().trim().to_string();
+        //     let authentication = columns.next().unwrap().text().trim().to_string();
+        //     let confidentiality = columns.next().unwrap().text().trim().to_string();
+        //     let integrity = columns.next().unwrap().text().trim().to_string();
+        //     let availability = columns.next().unwrap().text().trim().to_string();
+
+        //     let cve = Cve {
+        //         name: cve_name,
+        //         url: cve_url,
+        //         cwe_id: cwe.as_ref().map(|cwe| cwe.0.clone()),
+        //         cwe_url: cwe.as_ref().map(|cwe| cwe.1.clone()),
+        //         vulnerability_type,
+        //         publish_date,
+        //         update_date,
+        //         score,
+        //         access,
+        //         complexity,
+        //         authentication,
+        //         confidentiality,
+        //         integrity,
+        //         availability,
+        //     };
+        //     items.push(cve);
+        // }
+
+        // // find links to others pages
+        // let next_pages_links: Vec<String> = document
+        //     .find(Attr("id", "pagingb").descendant(Name("a")))
+        //     .filter_map(|n| n.attr("href"))
+        //     .map(normalize_url)
+        //     .collect();
+
+        Ok((items, cve_links))
     }
 
     async fn process(&self, item: Self::Item) -> Result<()> {
